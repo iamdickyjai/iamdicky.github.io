7ca44b5c7873fe382bf2296e48cc6819b1dd34e5
Turkey <soyiufai@gmail.com>
2021-07-05 16:56:40 +0800

    done RPS project

--- RockPS/app.js
+++ RockPS/app.js
@@ -13,87 +13,89 @@ function setUserChoice(choice) {
 function computerPlay() {
     computerChoice = Math.floor(Math.random() * 3) + 1;
     let output = "";
-    switch(computerChoice){
+    switch (computerChoice) {
         case 1:
-            output = "Computer choose Rock";
+            output = "img/rock.png";
             break;
         case 2:
-            output = "Computer choose Paper";
+            output = "img/paper.png";
             break;
         case 3:
-            output = "Computer choose Scissor";
+            output = "img/scissor.png";
             break;
     }
-    document.getElementById("computerChoice").innerHTML = output;
+    document.getElementById("computerChoice").src = output;
 }
 
 function compareResult() {
     let result = "";
-    if (userChoice<computerChoice) {
-        if(computerChoice - userChoice == 1){
+    if (userChoice < computerChoice) {
+        if (computerChoice - userChoice == 1) {
             result = "Player lose";
             console.log("lose");
             computerWinCount++;
         }
-        else{
+        else {
             result = "Player win";
             playerWinCount++;
         }
     }
-    else if (userChoice>computerChoice) {
-        if(userChoice - computerChoice == 1){
+    else if (userChoice > computerChoice) {
+        if (userChoice - computerChoice == 1) {
             result = "Player win";
             playerWinCount++;
         }
-        else{
+        else {
             result = "Player lose";
             console.log("lose");
             computerWinCount++;
         }
     }
-    else{
+    else {
         result = "Draw";
     }
 
     document.getElementById('result').innerHTML = result;
 }
 
-function calculateFinalResult(){
+function calculateFinalResult() {
     document.getElementById("currentResult").innerHTML = playerWinCount;
-    if(playerWinCount >= 5){
+    if (playerWinCount >= 5) {
         showBlock(2);
         document.getElementById("finalResult").innerHTML = "Player has win the battle";
     }
-    if(computerWinCount >= 5){
+    if (computerWinCount >= 5) {
         showBlock(2);
         document.getElementById("finalResult").innerHTML = "Computer has win the battle";
     }
 }
 
-function resetData(){
+function resetData() {
     userChoice = 0;
     computerChoice = 0;
     playerWinCount = 0;
     computerWinCount = 0;
+    document.getElementById("computerChoice").src = "";
+    document.getElementById("result").innerHTML = "";
     calculateFinalResult();
 }
 
-function showBlock(type){
+function showBlock(type) {
     let main = document.getElementById("main");
     let end = document.getElementById("end");
     let intro = document.getElementById("intro");
 
-    if(type == 1){
+    if (type == 1) {
         main.style.display = "block";
         intro.style.display = "none";
         end.style.display = "none";
     }
-    if(type == 2){
+    if (type == 2) {
         main.style.display = "none";
         intro.style.display = "none";
         end.style.display = "block";
     }
-    if(type==0){
+    if (type == 0) {
         main.style.display = "none";
         intro.style.display = "block";
         end.style.display = "none";
